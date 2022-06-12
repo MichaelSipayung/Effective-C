ffaf95be42fdbed8589d3fbdc685d6d9349da647
Michael Sipayung <michaelsipayung123@gmail.com>
2022-06-12 16:33:48 +0700

    chapter 7, more operation in string

--- chap7.c
+++ chap7.c
@@ -5,7 +5,11 @@
 #define S_INIT "abc"
 
 void using_siz_len(void);
-
+size_t efesien_strlen(const char *);
+void cp_str(void);
+char *imp_strcpy(char *,const char*);
+void get_y_or_n(void);
+char *imp_gets(char *);
 int main(void){
     char s[4]=S_INIT;
     printf("using array to store string literal: %s\n", s);
@@ -24,10 +28,52 @@ void using_siz_len(void){
     printf("wide character, test total character appear : %d\n", wcslen(wide_char));
     //narrow string, usefulness of the strlen function 
     char core_str[]="Hero comes to sun";
-    char al_str= malloc(strlen(core_str)+1);
+    char *al_str= malloc(strlen(core_str)+1);
     //wide string 
-    wchar_t wcore_str ="Heros comer to sun";
-    wchar_t wal_str = malloc((wcslen(wcore_str)+1) * sizeof(wchar_t));
-    char un_behavior[]="undifined \0 behavior";
-    printf("total character, test the behavior null charater: %d\n", strlen(un_behavior));
+    wchar_t wcore_str[] =L"Heros comer to sun";
+    wchar_t *wal_str = malloc((wcslen(wcore_str)+1) * sizeof(wchar_t));
+    //char un_behavior[]="undifined \0 behavior";
+    //printf("total character, test the behavior null charater: %d\n", strlen(un_behavior));
+    cp_str();
+}
+size_t efesien_strlen(const char *str){                     //another alternative for avoiding the undefenied behavior or strlen
+    const char *s;
+    for(s=str;*s;++s){}
+    return (s-str);
+}
+void cp_str(void){
+    char str[100]="Here comes the sun";
+    size_t str_size=strlen(str)+1;
+    char *dest =(char*)malloc(str_size);
+    if(dest){
+        strcpy(dest,str);
+    }
+    else{
+        printf("Error occurs while copy a string...\n");
+    }
+}
+char *imp_strcpy(char *dest,const char* src){
+    char *save = dest;
+    while (*dest++=*src);
+    return save;
+}
+void get_y_or_n(void){
+    //miuse of the obsolete gets function 
+    char resposnse[8];
+    puts("Continue ? [Y] n:");
+    //gets(resposnse);                                                 //compiler will warn you
+    if (resposnse[0]='n')
+    {
+        exit(0);
+    }
+    return;
+}
+char *imp_gets(char *dest){
+    int c ;
+    char *p =dest;                                                      //p points to same address with dest
+    while((c==getchar()) !=EOF && c!='\n'){
+        *p++=c;
+    }
+    *p='\0';
+    return dest;
 }
